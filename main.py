f8c6ca82f145e1560989cd6112c57f6e8fc27a4e
ℌ𝔞𝔰𝔰𝔞𝔫 ℜ𝔞𝔧𝔭𝔲𝔱 <135378730+HassanRajput0@users.noreply.github.com>
2024-09-19 21:21:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -107,4 +107,4 @@ def serve_appstate():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)u
+    app.run(host='0.0.0.0', port=port, debug=True)
