152a5d34b7c9a8b1399ea5fcd77c7b056b5acd3d
ℌ𝔞𝔰𝔰𝔞𝔫 ℜ𝔞𝔧𝔭𝔲𝔱 <135378730+HassanRajput0@users.noreply.github.com>
2024-09-19 21:24:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -106,5 +106,6 @@ def serve_appstate():
     return send_file('static/appstate.js')
 
 if __name__ == '__main__':
+    os.makedirs('uploads', exist_ok=True)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
